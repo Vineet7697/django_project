673253943db8dfbf532148aade5e2899aee444b5
vineet kushwaha <vineetkushwaha2010@gmail.com>
2025-10-17 23:01:52 +0530

    modified file

--- LandingPage/project/project/settings.py
+++ LandingPage/project/project/settings.py
@@ -9,11 +9,14 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# templates=BASE_DIR / 'templates'
+
+templates=os.path.join(BASE_DIR/ 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [templates],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
